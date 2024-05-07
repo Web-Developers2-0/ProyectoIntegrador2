0417567965e897d247ae0ced22bde833fd869afd
Vargas-Veronica <vargas.veronica55@gmail.com>
2024-05-07 10:51:09 -0300

    se logra visualizar los productos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,9 @@ import { ProductsComponent } from './products/products.component';
 export const routes: Routes = [
     //{path:"", redirectTo:"/home", pathMatch:"full"},
     {path:'app-products', component:ProductsComponent},
+    { path: '', redirectTo: '/products/marvel', pathMatch: 'full' },
+    { path: 'app-products/:category', component: ProductsComponent }
 ];
 
 
-    
 
