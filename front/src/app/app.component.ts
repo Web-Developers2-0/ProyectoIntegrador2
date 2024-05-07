93d32695cc02ac28623d11810d05efc139acc643
marcovirinni@gmail.com <marcovirinni@gmail.com>
2024-05-07 16:24:25 -0300

    fix routes, and waiting for new changes of master branch

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
+import { ProductsComponent } from './products/products.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet,HeaderComponent,ProductsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- front/src/app/shared/header/header.component.ts
+++ front/src/app/shared/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit} from '@angular/core';
 import {CommonModule, NgIf} from '@angular/common';
 import { Router, RouterLink, RouterModule,RouterLinkActive } from '@angular/router';
+import { ProductsComponent } from '../../products/products.component';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink,RouterModule,RouterLinkActive,NgIf, CommonModule],
+  imports: [RouterLink,RouterModule,RouterLinkActive,NgIf, CommonModule,ProductsComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
