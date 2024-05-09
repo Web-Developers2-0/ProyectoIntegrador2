de8f138723e462eb7a90338bf39e1fe10baef88b
Hernan <hernanzazzarini@gmail.com>
2024-05-09 03:42:16 +0200

    Se sube formulario de contacto

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,ProductsComponent],
+  imports: [RouterOutlet,HeaderComponent,ProductsComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
