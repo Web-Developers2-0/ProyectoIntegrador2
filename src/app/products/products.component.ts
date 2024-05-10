ccda90cf1d237662c70561d883cd8be1e667a5bb
Vargas-Veronica <vargas.veronica55@gmail.com>
2024-05-10 18:00:05 -0300

    se filtran y muetran ambos productos

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -11,59 +11,17 @@ import { RouterLink } from '@angular/router';
   styleUrl: './products.component.css'
 })
 
-
-export class ProductsComponent  {
-  /*selectedCategory: string = 'marvel';*/
-  productList:any
+export class ProductsComponent {
+  productList: any[] = [];
   selectedCategory: string = 'marvel';
-  constructor (private ProductServiceService:ProductServiceService) { 
-    console.log(" obtengo productos")
-  this.productList= ProductServiceService.obtenerProductos()
+
+  constructor(private productServiceService: ProductServiceService) { 
+    this.updateCategory(this.selectedCategory);
   }
 
-getFilteredProducts(): void {
-  console.log("me filtro")
-    this.productList.filter((product: { category: string; }) => product.category === this.selectedCategory);
-  }
-  f: any/*ilterProductsByCategory(category: string): void {
-    console.log("me filtro por categoria")
-   this.productList = this.productList.filter((product: { category: string; }) => product.category === category);
-}
-*/
-filterByCategory(category: string) {
-  let filteredProducts = this.productList.filter((product: { category: string; }) => product.category === category);
-  console.log(filteredProducts);
-}
-
-  
-  /*  filterProductsByCategory(category: string): any[] {
-    this.selectedCategory = 'category' || 'marvel'
-    return this.productList.filter(this.productList = this.productList.category === category);
-  }*/
   updateCategory(category: string): void {
     this.selectedCategory = category;
     console.log("me actualizo");
-    this.productList = this.productList.filter((product: { category: string; }) => product.category === this.selectedCategory);
-}
-
-  
-
-  redirectToDetail(detailPath: string): void {
-    window.location.href = detailPath;
+    this.productList = this.productServiceService.obtenerProductos().filter(product => product.category === this.selectedCategory);
   }
 }
-  /*ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      this.selectedCategory = params['category'] || 'marvel';
-      // Filtra los productos por la categoría seleccionada
-      this.products = this.filterProductsByCategory(this.selectedCategory);
-    });
-  }
-
-  
-
-  redirectToDetail(detailPath: string): void {
-    window.location.href = detailPath;
-  }
-
-}*/
