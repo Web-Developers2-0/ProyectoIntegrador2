559d04fb5bfce847840ff3936f3a2f4d6cf188d6
escuelamedia <escuelamedia86@gmail.com>
2024-05-11 12:39:47 -0300

    Html landing page

--- front/src/app/home/home.component.ts
+++ front/src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CarrouselComponent } from '../shared/carrousel/carrousel.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CarrouselComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
