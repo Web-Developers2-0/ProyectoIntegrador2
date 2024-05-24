fd8605b80b4043c010bcc756f4e3bd88957ca2f0
marcovirinni@gmail.com <marcovirinni@gmail.com>
2024-05-24 16:49:20 -0300

    Bearer config

--- back/universidad/universidad/settings.py
+++ back/universidad/universidad/settings.py
@@ -163,5 +163,13 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION':True,
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME':timedelta(days=1),
-    
+       
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',   
 }
