9e4b5be513ab8c1944927d3e56e213a4be93cb1e
marcovirinni@gmail.com <marcovirinni@gmail.com>
2024-06-02 20:39:20 -0300

    permissions for groups, prepared to merge develop

--- back/universidad/MyComicApp/initial_data.sql
+++ back/universidad/MyComicApp/initial_data.sql
@@ -197,7 +197,7 @@ VALUES
 
 
 
-('developers-superheroes@gmail.com', 'Develop', 'Team','pbkdf2_sha256$600000$TM6SUusGx9g4tG3ixjFULr$2D2mz/i5G1/1mEx4bhuIm6NKwPqmg3ZafjD0KD9SLFA=', 'localhost', 123456789, 'michael_brown.jpg', '2024-05-23 16:00:04', true, true, true, null);
+('developers-superheroes@gmail.com', 'Develop', 'Team','pbkdf2_sha256$600000$TM6SUusGx9g4tG3ixjFULr$2D2mz/i5G1/1mEx4bhuIm6NKwPqmg3ZafjD0KD9SLFA=', 'localhost', 123456789, 'michael_brown.jpg', '2024-05-23 16:00:04', true, true, true, 3);
 
 
 INSERT INTO `planetSuperheroesDB`.`orders` (user_id, state, order_date, payment_method, shipping_method, payment_status, total_amount)
--- front/src/app/services/order.service.ts
+++ front/src/app/services/order.service.ts
@@ -1,17 +1,1 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Order,OrderItem } from './order.interface';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class OrderService {
-  private apiUrl = 'http://127.0.0.1:8000/api/orders/create/';
-
-  constructor(private http: HttpClient) {}
-
-  createOrder(order: Order): Observable<Order> {
-    return this.http.post<Order>(this.apiUrl, order);
-  }
-}
